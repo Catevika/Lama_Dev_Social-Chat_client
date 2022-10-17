6e9536883077f2cca7bcbd0cc182cd2713487557
Catevika <dominique.bello@outlook.fr>
2022-10-17 15:56:32 +0200

    Update Post & Feed with dummyData

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -1,17 +1,16 @@
 import Post from '../post/Post';
 import SharePost from '../sharePost/SharePost';
 import './feed.css';
+import { Posts } from '../../dummyData';
 
 export default function Feed() {
 	return (
 		<div className='feed-container'>
 			<div className='feed-wrapper'>
 				<SharePost />
-				<Post />
-				<Post />
-				<Post />
-				<Post />
-				<Post />
+				{Posts.map((post) => (
+					<Post key={post.id} post={post} />
+				))}
 			</div>
 		</div>
 	);
--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,20 +1,40 @@
+import { useState } from 'react';
+import { Users } from '../../dummyData';
 import { MoreVert } from '@mui/icons-material';
 import './post.css';
 
-export default function Post() {
+export default function Post({ post }) {
+	const [like, setLike] = useState(post.like);
+	const [isLiked, setIsLiked] = useState(false);
+	const [love, setLove] = useState(post.love);
+	const [isLoved, setIsLoved] = useState(false);
+
+	const handleLike = () => {
+		setLike(isLiked ? like - 1 : like + 1);
+		setIsLiked(!isLiked);
+	};
+	const handleLove = () => {
+		setLove(isLoved ? love - 1 : love + 1);
+		setIsLoved(!isLoved);
+	};
 	return (
 		<div className='post-container'>
 			<div className='post-wrapper'>
 				<div className='post-top'>
 					<div className='post-top-left'>
 						<img
-							src='/assets/person/0.png'
+							src={
+								Users.filter((user) => user.id === post.userId)[0]
+									.profilePicture
+							}
 							alt=''
 							className='post-profile-img'
 						/>
 						<div className='post-img-bottom'>
-							<span className='post-username'>Catevika</span>
-							<span className='post-date'>5 mins ago</span>
+							<span className='post-username'>
+								{Users.filter((user) => user.id === post.userId)[0].username}
+							</span>
+							<span className='post-date'>{post.date}</span>
 						</div>
 					</div>
 					<div className='post-top-right'>
@@ -22,8 +42,8 @@ export default function Post() {
 					</div>
 				</div>
 				<div className='post-center'>
-					<img src='/assets/post/1.jpeg' alt='' className='post-img' />
-					<span className='post-text'>Hey here is my 1st post!</span>
+					<img src={post.photo} alt='' className='post-img' />
+					<span className='post-text'>{post?.desc}</span>
 				</div>
 				<div className='post-bottom'>
 					<div className='post-bottom-left'>
@@ -31,16 +51,22 @@ export default function Post() {
 							src='/assets/like.png'
 							alt=''
 							className='post-bottom-left-icon'
+							onClick={handleLike}
 						/>
 						<img
 							src='/assets/heart.png'
 							alt=''
 							className='post-bottom-left-icon'
+							onClick={handleLove}
 						/>
-						<span className='post-like-counter'>32 people like it</span>
+						<span className='post-like-counter'>
+							<b>Like:</b> {like} - <b>Love:</b> {love}
+						</span>
 					</div>
 					<div className='post-bottom-right'>
-						<span className='post-comment-counter'>9 comments</span>
+						<span className='post-comment-counter'>
+							{post.comment} comments
+						</span>
 					</div>
 				</div>
 			</div>
